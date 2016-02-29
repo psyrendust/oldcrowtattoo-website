cf73a51e8f85e89ba22e74c5e4f280d9fecffcea
Larry Gordon <lgordon@psyrendust.com>
2016-02-29 00:56:25 -0800

    Release

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function IIFE($) {
+  $(document).ready(function () {
+    $(document).foundation();
+    console.log('yo yo hello'); // eslint-disable-line
+  });
+})(jQuery);
+//# sourceMappingURL=app.js.map
